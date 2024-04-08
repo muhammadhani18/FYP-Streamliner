423ad114babb0da18a752d56f7b279fbd0783706
muhammadhani18 <mhani18403@gmail.com>
2024-04-08 16:43:32 +0500

    Add FYP page

---
+++ Pages/AddFYP.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SE_Project.Pages
+{
+    public class AddFYPModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
